437dbff5d074733335e338ca342ae94d6f20af2d
punzalpatrickjoseph <punzalpatrickjoseph17@gmail.com>
2024-10-30 21:14:35 +0800

    classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,8 @@
+let subTitle = "DSA";
+let classSchedule = "8-9:30 am, MF"
+let classRoom = "IR";
+let classInstructor = "MR. Cliff Owen Pascua";
+let studentName = "Patrick Joseph Punzal";
+let userInfo = studentName +" is currently enrolled in "+ subTitle +" with a class schedule of "+
+  classSchedule +" at room "+ classRoom +" . "+ "The Instructor for the subject is "+ classInstrucctor
+console.log(userInfo);
