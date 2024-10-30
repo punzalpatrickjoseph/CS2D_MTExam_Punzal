7d489cc72b6f93f5fe09c743dc61d6b86c401e05
punzalpatrickjoseph <punzalpatrickjoseph17@gmail.com>
2024-10-30 20:51:20 +0800

    userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Patrick Joseph Punzal";
+let age = 22;
+let favNumber = 26;
+let favcolor = "Red";
+
+console.log(fulName);
+console.loh(age);
+console.log(favoritenumber);
+console.log(favoritecolor);
